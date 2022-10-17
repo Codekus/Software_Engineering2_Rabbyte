4caba27d1c2bdffbb1776a22f9756af88ea0f5a0
lm <lars.michaelis@smail.inf.h-brs.de>
2022-10-17 22:10:17 +0200

    Created basic structure bases on carlook and created a MainView

---
+++ src/main/java/de/hbrs/se/Rabbyte/RabbyteApplication.java
@@ -0,0 +1,13 @@
+package de.hbrs.se.Rabbyte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbyteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbyteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/hbrs/se/Rabbyte/RabbyteApplicationTests.java
@@ -0,0 +1,13 @@
+package de.hbrs.se.Rabbyte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbyteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
