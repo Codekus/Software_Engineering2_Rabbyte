6495f04705b6a0b2023376e1fbda502465036dd0
lm <lars.michaelis@smail.inf.h-brs.de>
2022-10-18 11:37:08 +0200

    Setting Route

--- src/main/java/de/hbrs/se/Rabbyte/views/MainView.java
+++ src/main/java/de/hbrs/se/Rabbyte/views/MainView.java
@@ -5,12 +5,14 @@ import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.router.RouteAlias;
 import com.vaadin.flow.server.PWA;
 
 
 
 @Route(value = "" )
-@PWA(name = "My Application", shortName = "My Application")
+@RouteAlias(value = "main" )
+@PWA(name = "Rabbyte", shortName = "Rabbyte")
 public class MainView extends VerticalLayout {
 
     public MainView() {
