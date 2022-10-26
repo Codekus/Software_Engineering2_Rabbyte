8a42d0486f582eb1c7f8402ee97fbc1da4c7e229
lm <lars.michaelis@smail.inf.h-brs.de>
2022-10-26 22:39:56 +0200

    Autowired Repositories

--- src/main/java/de/hbrs/se/rabbyte/control/RegistrationControl.java
+++ src/main/java/de/hbrs/se/rabbyte/control/RegistrationControl.java
@@ -1,5 +1,8 @@
 package de.hbrs.se.rabbyte.control;
 
+import de.hbrs.se.rabbyte.repository.BusinessRepository;
+import de.hbrs.se.rabbyte.repository.GeneralUserRepository;
+import de.hbrs.se.rabbyte.repository.StudentRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +10,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 public class RegistrationControl {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(RegistrationControl.class);
+
+
+    @Autowired
+    GeneralUserRepository generalUserRepository;
+
+    @Autowired
+    StudentRepository studentRepository;
+
+    @Autowired
+    BusinessRepository businessRepository;
 
 
 }
