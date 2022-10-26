4011d2ad2713bed541dc8713f8509bceec232ca1
lm <lars.michaelis@smail.inf.h-brs.de>
2022-10-26 22:36:05 +0200

    Created RegistrationControl

---
+++ src/main/java/de/hbrs/se/rabbyte/control/RegistrationControl.java
@@ -0,0 +1,12 @@
+package de.hbrs.se.rabbyte.control;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class RegistrationControl {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(RegistrationControl.class);
+
+
+}
