767e188eeb2c6ce7a65bf94468e5b5e9543fa726
lm <lars.michaelis@smail.inf.h-brs.de>
2022-11-02 16:15:25 +0100

    Testing UserFactory

---
+++ src/test/java/de/hbrs/se/rabbyte/control/factory/UserFactoryTest.java
@@ -0,0 +1,61 @@
+package de.hbrs.se.rabbyte.control.factory;
+
+import de.hbrs.se.rabbyte.dtos.BusinessDTO;
+import de.hbrs.se.rabbyte.dtos.StudentDTO;
+import de.hbrs.se.rabbyte.entities.Business;
+import de.hbrs.se.rabbyte.entities.Student;
+import de.hbrs.se.rabbyte.entities.User;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+@ExtendWith(MockitoExtension.class)
+class UserFactoryTest {
+
+    @Mock
+    private StudentDTO studentDTO;
+
+    @Mock
+    private BusinessDTO businessDTO;
+    @Mock
+    private User user;
+
+    @Test
+    void createStudent() {
+        Student student;
+        when(studentDTO.getPassword()).thenReturn("password");
+        when(studentDTO.getEmail()).thenReturn("max@mustermann.de");
+        when(studentDTO.getFirstName()).thenReturn("Max");
+        when(studentDTO.getLastName()).thenReturn("Mustermann");
+
+
+        student = UserFactory.createStudent(studentDTO);
+
+        assertTrue(student instanceof Student);
+        assertEquals("password" , student.getPassword());
+        assertEquals("max@mustermann.de" , student.getEmail());
+        assertEquals("Max" , student.getFirstName());
+        assertEquals("Mustermann" , student.getLastName());
+
+    }
+
+    @Test
+    void createBusiness() {
+        Business business;
+        String password = "password";
+        when(businessDTO.getPassword()).thenReturn(password);
+        when(businessDTO.getEmail()).thenReturn("musterman@gmbh.de");
+        when(businessDTO.getBusinessName()).thenReturn("Mustermann Gmbh");
+
+        business = UserFactory.createBusiness(businessDTO);
+        assertEquals("password" , business.getPassword());
+        assertEquals("musterman@gmbh.de" , business.getEmail());
+        assertEquals("Mustermann Gmbh" , business.getBusinessName());
+
+    }
+}
