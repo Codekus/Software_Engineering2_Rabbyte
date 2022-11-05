9a8e1eb2b054b354897a83b7da72b67499c9a3fa
lm <lars.michaelis@smail.inf.h-brs.de>
2022-11-05 20:45:10 +0100

    When a User is created a salt value is also saves as Hex in the Database

--- src/main/java/de/hbrs/se/rabbyte/control/factory/UserFactory.java
+++ src/main/java/de/hbrs/se/rabbyte/control/factory/UserFactory.java
@@ -1,24 +1,35 @@
 package de.hbrs.se.rabbyte.control.factory;
 
+import de.hbrs.se.rabbyte.control.RegistrationControl;
 import de.hbrs.se.rabbyte.dtos.BusinessDTO;
 import de.hbrs.se.rabbyte.dtos.StudentDTO;
 import de.hbrs.se.rabbyte.entities.Business;
 import de.hbrs.se.rabbyte.entities.Student;
 import de.hbrs.se.rabbyte.util.CryptographyUtil;
+import de.hbrs.se.rabbyte.util.Utils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.security.NoSuchAlgorithmException;
 
 public class UserFactory {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(UserFactory.class);
+
     static CryptographyUtil cryptographyUtil = new CryptographyUtil();
     private UserFactory() {
         throw new IllegalStateException("Factory Class");
     }
-    public static Student createStudent(StudentDTO studentDTO) throws NoSuchAlgorithmException {
+    public static Student createStudent(StudentDTO studentDTO)  {
         Student student = new Student();
+        try {
+            byte[] salt = CryptographyUtil.generateSalt();
+            student.setSalt(CryptographyUtil.toHex(salt));
+            student.setPassword(cryptographyUtil.encryptPassword(studentDTO.getPassword() , salt));
+        } catch (NoSuchAlgorithmException exception) {
+            LOGGER.info(exception.getMessage());
+        }
 
-
-        student.setPassword(cryptographyUtil.encryptPassword(studentDTO.getPassword()).toString());
         student.setEmail(studentDTO.getEmail());
         student.setFirstName(studentDTO.getFirstName());
         student.setLastName(studentDTO.getLastName());
@@ -29,7 +40,13 @@ public class UserFactory {
     public static Business createBusiness(BusinessDTO businessDTO) {
         Business business = new Business();
 
-        business.setPassword(businessDTO.getPassword());
+        try {
+            byte[] salt = CryptographyUtil.generateSalt();
+            business.setSalt(CryptographyUtil.toHex(salt));
+            business.setPassword(businessDTO.getPassword());
+        } catch (NoSuchAlgorithmException exception) {
+            LOGGER.info(exception.getMessage());
+        }
         business.setEmail(businessDTO.getEmail());
         business.setBusinessName(businessDTO.getBusinessName());
 
