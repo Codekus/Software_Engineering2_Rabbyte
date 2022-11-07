bf793a35d176da7c9b57bcdb44fc4eed81b38ef7
lm <lars.michaelis@smail.inf.h-brs.de>
2022-11-08 00:42:14 +0100

    Tests

--- src/test/java/de/hbrs/se/rabbyte/repository/GeneralUserRepositoryTest.java
+++ src/test/java/de/hbrs/se/rabbyte/repository/GeneralUserRepositoryTest.java
@@ -1,23 +1,15 @@
 package de.hbrs.se.rabbyte.repository;
 
-import de.hbrs.se.rabbyte.dtos.GeneralUserDTO;
-
 import io.zonky.test.db.AutoConfigureEmbeddedDatabase;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.jdbc.Sql;
-import org.springframework.test.context.junit4.SpringRunner;
-
-import static io.zonky.test.db.AutoConfigureEmbeddedDatabase.DatabaseProvider.ZONKY;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @DataJpaTest
 @AutoConfigureEmbeddedDatabase(provider = AutoConfigureEmbeddedDatabase.DatabaseProvider.ZONKY )
-@Sql({"/rabbyte_schema.sql", "/rabbyte_data.sql"})
+@Sql(scripts = {"/rabbyte_schema.sql", "/rabbyte_data.sql"})
 @AutoConfigureEmbeddedDatabase
 
 class GeneralUserRepositoryTest {
