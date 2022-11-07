12d55128b1effd27e4e61ba1bc297a71d2fdb04c
lm <lars.michaelis@smail.inf.h-brs.de>
2022-11-08 00:09:08 +0100

    Testing Repository

---
+++ src/test/ressources/rabbyte_data.sql
@@ -0,0 +1,6 @@
+--
+-- Data for Name: user; Type: TABLE DATA; Schema: rabbyte; Owner: ihbib2s
+--
+
+INSERT INTO rabbyte.user (user_id, email, passwort, plz, stadt, land, strasse, hausnummer, salt)  VALUES (20000086 , 'max@gmx.de' , '8e0ac6815b51b73afd18c0ffd5d1eb0cdd3fb8ece16ad685f9199b1c4b55435bfde9ba85616ceee5072418c76d0de5086a0ae002f0be1665cd33a5af33b2f24f' , 12345 , 'Musterstadt' , 'DE' , 'Musterstadt' , '5' , '0a7031361558cd761bb1fda3e4b7367d7bf4b04930c8bad4da16b1b46648428366f1376f326c8a8cedc6a73d7c55e442e4c3ffa0a515d9190c770cffb2c56d73');
+
