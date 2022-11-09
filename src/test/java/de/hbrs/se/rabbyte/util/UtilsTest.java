43cc8016acf55fcf8ba86f2699161a1f2ad1f732
lm <lars.michaelis@smail.inf.h-brs.de>
2022-11-09 17:57:46 +0100

    added tests

--- src/test/java/de/hbrs/se/rabbyte/util/UtilsTest.java
+++ src/test/java/de/hbrs/se/rabbyte/util/UtilsTest.java
@@ -1,6 +1,9 @@
 package de.hbrs.se.rabbyte.util;
 
+import com.vaadin.flow.component.UI;
+import com.vaadin.flow.server.VaadinSession;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Modifier;
@@ -10,12 +13,36 @@ import static org.junit.jupiter.api.Assertions.*;
 class UtilsTest {
 
     private static final String exceptionMessage = "class de.hbrs.se.rabbyte.util.UtilsTest cannot access a member of class de.hbrs.se.rabbyte.util.Utils with modifiers \"private\"";
+
     @Test
-    void append() {
+    void appendLength() {
+        Integer[] numberArray = {1, 2, 3};
+        Integer[] numberArrayAppended = Utils.append(numberArray, 4);
+        assertEquals(numberArray.length + 1, numberArrayAppended.length);
     }
+
+
+    @Test
+    void appendCheckObjectAppended() {
+
+        Integer[] numberArray = {1, 2, 3};
+        Integer[] numberArrayAppended = Utils.append(numberArray, 4);
+        assertEquals(4, numberArrayAppended[numberArrayAppended.length - 1]);
+
+    }
+
 
     @Test
     void triggerDialogMessage() {
+
+        UI ui = new UI();
+        UI.setCurrent(ui);
+
+        VaadinSession session = Mockito.mock(VaadinSession.class);
+        Mockito.when(session.hasLock()).thenReturn(true);
+        ui.getInternals().setSession(session);
+        Utils.triggerDialogMessage("headerText" , "message");
+        UI.setCurrent(null);
     }
 
     @Test
