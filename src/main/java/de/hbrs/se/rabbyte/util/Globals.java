c689f95cac654ff11791c4dd22d91fe776d02e4b
lm <lars.michaelis@smail.inf.h-brs.de>
2022-11-13 13:33:47 +0100

    updated Business Regex

--- src/main/java/de/hbrs/se/rabbyte/util/Globals.java
+++ src/main/java/de/hbrs/se/rabbyte/util/Globals.java
@@ -31,7 +31,7 @@ public class Globals {
 
     public static class Regex {
         private Regex() { throw new IllegalStateException("Utility");}
-        public static final String BUSINESS_NAME = "/^[a-zA-Z0-9-@.{}#&!()]+(\\s[a-zA-Z0-9-@{}.#&!()]+)+(\\s[a-zA-Z-@.#&!()]+)?$/";
+        public static final String BUSINESS_NAME = "^[a-zA-Z0-9& ]*{2,40}$";
         public static final String EMAIL = "^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^-]+(?:\\.[a-zA-Z0-9_!#$%&'*+/=?`{|}~^-]+)*@"
                 + "[a-zA-Z0-9-]+(?:\\.[a-zA-Z0-9-]+)*$";
         public static final String LAST_NAME = "^([^\\p{N}\\p{S}\\p{C}\\p{P}]{2,20})$";
