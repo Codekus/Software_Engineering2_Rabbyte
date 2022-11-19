f7ad45e415d78aded810b49180f21657c0e98c1f
lm <lars.michaelis@smail.inf.h-brs.de>
2022-11-20 00:35:53 +0100

    added test

--- src/test/java/de/hbrs/se/rabbyte/control/ActivationControlTest.java
+++ src/test/java/de/hbrs/se/rabbyte/control/ActivationControlTest.java
@@ -1,11 +1,18 @@
 package de.hbrs.se.rabbyte.control;
 
+import de.hbrs.se.rabbyte.entities.VerificationCode;
 import de.hbrs.se.rabbyte.repository.VerificationCodeRepository;
+import io.zonky.test.db.AutoConfigureEmbeddedDatabase;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.jdbc.Sql;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+@DataJpaTest
+@AutoConfigureEmbeddedDatabase(provider = AutoConfigureEmbeddedDatabase.DatabaseProvider.ZONKY )
+@Sql(scripts = {"file:src/test/ressources/rabbyte_schema.sql ", "file:src/test/ressources/rabbyte_data.sql"})
+@AutoConfigureEmbeddedDatabase
 class ActivationControlTest {
 
     @Autowired
@@ -16,9 +23,10 @@ class ActivationControlTest {
     }
 
     @Test
-    void getVerificationCode() {
+    void getVerificationCodeNullCheck() {
 
-        assertEquals(200 , verificationCodeRepository.findVerificationCodeById(60000018));
+        assertNotNull(verificationCodeRepository.findVerificationCodeById(60000017));
+        assertNotNull(verificationCodeRepository.findVerificationCodeByToken("606728a3-f4dd-4a12-a75d-1411773e25b7"));
 
     }
 }
