bf60b1aac2e97ae632c169c972608ddab0c72f6f
Issam Hbib <issam.hbib@smail.inf.h-brs.de>
2022-11-20 15:20:49 +0100

    After login setting route for root host

--- src/main/java/de/hbrs/se/rabbyte/security/SecurityService.java
+++ src/main/java/de/hbrs/se/rabbyte/security/SecurityService.java
@@ -115,9 +115,11 @@ public class SecurityService  {
         if (Objects.equals(getRole(user), "Student")){
             routes.add(new AuthorizedRoute("student", "Student", StudentUserView.class));
             routes.add(new AuthorizedRoute("main", "Search Job Advertisement", JobAdvertisementSearchView.class));
+            routes.add(new AuthorizedRoute("", "Search Job Advertisement", JobAdvertisementSearchView.class));
         } else if (Objects.equals(getRole(user), "Business")) {
             routes.add(new AuthorizedRoute("jobAd", "Create Job Advertisement", CreateJobAdvertisementView.class));
             routes.add(new AuthorizedRoute("main", "Business", BusinessView.class));
+            routes.add(new AuthorizedRoute("", "Business", BusinessView.class));
 
         }
         return routes;
