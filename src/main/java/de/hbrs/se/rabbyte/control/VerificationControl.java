17600442bb2304d7ff9ceffeb21c00a193a235b1
lm <lars.michaelis@smail.inf.h-brs.de>
2022-11-20 12:50:09 +0100

    deleted useless class

--- src/main/java/de/hbrs/se/rabbyte/control/VerificationControl.java
+++
@@ -1,9 +0,0 @@
-package de.hbrs.se.rabbyte.control;
-
-import de.hbrs.se.rabbyte.dtos.implemented.VerificationCodeDTOImpl;
-import de.hbrs.se.rabbyte.entities.Student;
-
-
-public class VerificationControl {
-
-}
