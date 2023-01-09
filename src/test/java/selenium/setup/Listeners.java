497c098e366bed6a3b82f16189d3cdef5d434799
Codekus <markusklassen1497@yahoo.de>
2023-01-09 20:56:22 +0100

    Listener updated

--- src/test/java/selenium/setup/Listeners.java
+++ src/test/java/selenium/setup/Listeners.java
@@ -15,18 +15,14 @@ public class Listeners extends TestSetup implements ITestListener {
     }
 
     public void onTestSuccess(ITestResult result){
-        try {
-            File destFile = TestSetup.takeScreenshots(result.getName());
-            System.out.println("before");
-            Reporter.log("<a href='../../tmp/uploads/screenshots/" + destFile.getName() +
-                    "'> <img src='../../tmp/uploads/screenshots/" + destFile.getName() + "' height='100' width='100'/> </a>");
-            System.out.println("after");
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        logScreenshot(result);
     }
 
     public void onTestFailure(ITestResult result){
+        logScreenshot(result);
+    }
+
+    private void logScreenshot(ITestResult result){
         try {
             File destFile = TestSetup.takeScreenshots(result.getName());
             Reporter.log("<a href='../../tmp/uploads/screenshots/" + destFile.getName() +
